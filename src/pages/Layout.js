3d8e387e0473a7b08bfbaed5fdcfd9dbfc127e5c
Paulo Camopy <camopy@Paulos-MacBook-Pro.local>
2016-10-11 01:30:44 -0300

    limpa cache baseado na versao

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -2,9 +2,28 @@ import React, { Component } from 'react';
 
 import Header from "../components/layout/Header"
 import Footer from "../components/layout/Footer"
+import * as HeaderActions from "../actions/HeaderActions";
 
 class Layout extends Component {
+  constructor() {
+    super()
+    this.versao = "1.0.0";
+  }
+
+  getVersao(){
+    return localStorage.getItem("versaoFecharConta");
+  }
+
+  setVersao(){
+    localStorage.setItem("versaoFecharConta", this.versao);
+  }
+
   render() {
+    if(this.versao !== this.getVersao()){
+      this.setVersao();
+      HeaderActions.clearData();
+    }
+
     return (
       <div>                
         <Header/>
